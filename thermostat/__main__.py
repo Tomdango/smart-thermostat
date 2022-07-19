a050f037b6abf0855b8bb9c1e1671bbbbd195717
Thomas Judd-Cooper <me@tomjuddcooper.co.uk>
2022-07-19 23:24:25 +0100

    Just turn it off

--- thermostat/__main__.py
+++ thermostat/__main__.py
@@ -13,7 +13,7 @@ class State:
     _brightness: float = 1.0
     _power_saving_mode_enabled: bool = False
 
-    led_colour: Tuple[float, float, float] = (0.01, 0.01, 0.01)
+    led_colour: Tuple[float, float, float] = (0.0, 0.0, 0.0)
 
     @property
     def brightness(self):
